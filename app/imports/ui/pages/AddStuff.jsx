0662bb60313db0ea7cf8e222c371f22a73bc9654
jishimaru2 <jishimar@hawaii.edu>
2021-11-12 16:29:08 -1000

    issue-05: initial commit

--- app/imports/ui/pages/AddStuff.jsx
+++ app/imports/ui/pages/AddStuff.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Grid, Segment, Header } from 'semantic-ui-react';
+import { Grid, Segment, Header, Card, Image, Icon } from 'semantic-ui-react';
 import { AutoForm, ErrorsField, NumField, SelectField, SubmitField, TextField } from 'uniforms-semantic';
 import swal from 'sweetalert';
 import { Meteor } from 'meteor/meteor';
@@ -41,20 +41,55 @@ class AddStuff extends React.Component {
   // Render the form. Use Uniforms: https://github.com/vazco/uniforms
   render() {
     let fRef = null;
+    const gridStyle = { height: '500px' };
     return (
-      <Grid container centered>
-        <Grid.Column>
-          <Header as="h2" textAlign="center">Add Stuff</Header>
-          <AutoForm ref={ref => { fRef = ref; }} schema={bridge} onSubmit={data => this.submit(data, fRef)} >
-            <Segment>
-              <TextField name='name'/>
-              <NumField name='quantity' decimal={false}/>
-              <SelectField name='condition'/>
-              <SubmitField value='Submit'/>
-              <ErrorsField/>
-            </Segment>
-          </AutoForm>
-        </Grid.Column>
+      <Grid container verticalAlign="middle" style={gridStyle}>
+        <Grid.Row columns="two">
+          <Grid.Column>
+            <Card>
+              <Image src='https://pyxis.nymag.com/v1/imgs/fdf/db0/421af17121276a71945282e440357c43cb-25-post-malone-2.rsquare.w700.jpg' wrapped ui={false} />
+              <Card.Content>
+                <Card.Header>Post-Malone</Card.Header>
+                <Card.Meta>Hip-Hop/Rap</Card.Meta>
+                <Card.Description>
+                  American rapper, singer, songwriter, record producer
+                </Card.Description>
+              </Card.Content>
+              <Card.Content extra>
+                <a>
+                  <Icon name='spotify' />
+                </a>
+                <a>
+                  <Icon name='soundcloud' />
+                </a>
+                <a>
+                  <Icon name='youtube' />
+                </a>
+              </Card.Content>
+            </Card>
+          </Grid.Column>
+          <Grid.Column>
+            <Header as="h2" textAlign="center">Post Malone</Header>
+            <AutoForm ref={ref => { fRef = ref; }} schema={bridge} onSubmit={data => this.submit(data, fRef)} >
+              <Segment>
+                <Segment vertical>
+                  Name: Post Malone
+                </Segment>
+                <Segment vertical>
+                  Goals: Movie Star
+                </Segment>
+                <Segment vertical>
+                  Instruments: Guitar
+                </Segment>
+                <Segment vertical>
+                  Genres: Hip-hop/Rap
+                </Segment>
+                <SubmitField value='Back to Profiles'/>
+                <ErrorsField/>
+              </Segment>
+            </AutoForm>
+          </Grid.Column>
+        </Grid.Row>
       </Grid>
     );
   }
