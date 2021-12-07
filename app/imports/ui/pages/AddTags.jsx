e19cce8bb7cac5ffd4bf2a8247b1af60af35e5c5
Coltin-Kai <coltinkaikaupiko@gmail.com>
2021-12-06 15:19:18 -1000

    AddTags Page Fixed

--- app/imports/ui/pages/AddTags.jsx
+++ app/imports/ui/pages/AddTags.jsx
@@ -1,10 +1,37 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
-import { Container, Dropdown, Form, Header, Input, Loader, Message } from 'semantic-ui-react';
+import { Container, Dropdown, Header, Input, Loader, Message, Button } from 'semantic-ui-react';
 import { withTracker } from 'meteor/react-meteor-data';
 import PropTypes from 'prop-types';
 import swal from 'sweetalert';
 import { Tags } from '../../api/tags/Tags';
+
+function ValidateName(props) {
+  // eslint-disable-next-line react/prop-types
+  if (props.alreadyIn) {
+    return (
+      <Message warning>
+        {/* eslint-disable-next-line react/prop-types */}
+        <Message.Header>Tag already in {props.tag}</Message.Header>
+        <p>Please enter a new tag name.</p>
+      </Message>
+    );
+    // eslint-disable-next-line react/prop-types
+  } if (props.inputEmpty) {
+    return (
+      <Message>
+        <Message.Header>Please write the name of the tag</Message.Header>
+      </Message>
+    );
+  }
+  return (
+    <Button onClick={() => {
+      // eslint-disable-next-line react/prop-types
+      Tags.collection.update(Tags.collection.findOne()._id, { $set: { [props.tag]: props.arrayToAdd } });
+      swal('Success', 'Tag Added', 'success');
+    }}>Add</Button>
+  );
+}
 
 /** Renders a table containing all of the Stuff documents. Use <StuffItem> to render each row. */
 class AddTags extends React.Component {
@@ -14,26 +41,6 @@ class AddTags extends React.Component {
   currentTag = 'instruments';
 
   setTagName = (e, input) => this.setState({ name: input.value });
-
-  validateName() {
-    if (this.props.tags[0][this.currentTag].find((tag) => tag === this.state.name)) {
-      return (
-        <Message warning>
-          <Message.Header>Tag Already In {this.currentTag}</Message.Header>
-          <p>Please Enter A New Tag Name.</p>
-        </Message>
-      );
-    }
-    else {
-      return (
-        <Button onClick={this.submit}>Add</Button>
-      );
-    }
-  }
-
-  submit() {
-    Tags.collection.update()
-  }
 
   // If the subscription(s) have been received, render the page, otherwise show a loading icon.
   render() {
@@ -60,16 +67,24 @@ class AddTags extends React.Component {
         value: 'capabilities',
       },
     ];
+    const result = [...this.props.tags[0][this.currentTag]];
+    result.push(this.state.name);
+    let alreadyIn = true;
+    if (this.props.tags[0][this.currentTag].indexOf(this.state.name) === -1) {
+      alreadyIn = false;
+    }
+    let inputEmpty = false;
+    if (this.state.name.length < 1) {
+      inputEmpty = true;
+    }
     return (
       <Container>
         <Header as="h2" textAlign="center">Add Tags</Header>
-        <Dropdown selection defaultValue="Instruments" options={availableTags} onChange={(e, dropdown) => {
-          console.log(this.currentTag);
+        <Dropdown selection placeholder="Instruments" defaultValue="instruments" options={availableTags} onChange={(e, dropdown) => {
           this.currentTag = dropdown.value;
-          console.log(this.currentTag);
         }}/>
         <Input placeholder="Tag Name" onChange={this.setTagName}/>
-        {this.validateName}
+        <ValidateName alreadyIn={alreadyIn} tag={this.currentTag} arrayToAdd={result} inputEmpty={inputEmpty}/>
       </Container>
     );
   }
