4412b78b8b2f840b85b931dcb2b2c9ea5882a48a
AnatAmar1 <aa91@hawaii.edu>
2021-12-09 11:20:39 -1000

    clean-up

--- app/imports/ui/components/NavBar.jsx
+++ app/imports/ui/components/NavBar.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Meteor } from 'meteor/meteor';
 import { withTracker } from 'meteor/react-meteor-data';
-import { withRouter, NavLink } from 'react-router-dom';
-import { Menu, Dropdown, Header, Image } from 'semantic-ui-react';
+import { NavLink, withRouter } from 'react-router-dom';
+import { Dropdown, Header, Image, Menu } from 'semantic-ui-react';
 import { Roles } from 'meteor/alanning:roles';
 
 /** The NavBar appears at the top of every page. Rendered by the App Layout component. */
@@ -16,15 +16,17 @@ class NavBar extends React.Component {
           <Header inverted as='h1'>Manoa Music <Image size="mini" src="https://www.clipartmax.com/png/full/15-153553_green-music-notes-clipart-music-note-clip-art.png"/></Header>
         </Menu.Item>
         {this.props.currentUser ? (
-          [<Menu.Item id = 'navbar-create-profile-page' as={NavLink} activeClassName="active" exact to="/add" key='add'>Create Profile</Menu.Item>,
+          [
             <Menu.Item as={NavLink} activeClassName="active" exact to="/createsession" key='add'>Create Jam Session</Menu.Item>,
             <Menu.Item as={NavLink} activeClassName="active" exact to="/listjamsessions" key='add'>List Jam Sessions</Menu.Item>,
-            <Menu.Item as={NavLink} activeClassName="active" exact to="/create" key='create'>Create Profile Template</Menu.Item>,
             <Menu.Item id="navbar-user-home" as={NavLink} activeClassName="active" exact to="/userhome" key='user'>List Profiles</Menu.Item>,
-            <Menu.Item id="navbar-list-profiles-page" as={NavLink} activeClassName="active" exact to="/list" key='list'>List Profiles New</Menu.Item>]
+          ]
         ) : ''}
         {Roles.userIsInRole(Meteor.userId(), 'admin') ? (
-          <Menu.Item id="navbar-list-profiles-admin-page" as={NavLink} activeClassName="active" exact to="/adminlist" key='admin'>List Profiles (Admin)</Menu.Item>
+          [
+            <Menu.Item id="navbar-list-profiles-admin-page" as={NavLink} activeClassName="active" exact to="/adminlist" key='admin'>List Profiles (Admin)</Menu.Item>,
+            <Menu.Item as={NavLink} activeClassName="active" exact to="/addTags" key='admin'>Add Tags (Admin)</Menu.Item>,
+          ]
         ) : ''}
         <Menu.Item position="right">
           {this.props.currentUser === '' ? (
